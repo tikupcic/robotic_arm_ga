87f33f028baa69152d7830a0e83b321220da731d
TK <timi.kupcic@gmail.com>
2018-04-30 20:01:15 +0200

    feat: clion inital build files ("it begins!")

---
+++ cmake-build-debug/CMakeFiles/robotic_arm_ga.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robotic_arm_ga.dir/main.cpp.o"
+  "robotic_arm_ga.pdb"
+  "robotic_arm_ga.exe"
+  "librobotic_arm_ga.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robotic_arm_ga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "It begins!" << std::endl;
+    return 0;
+}
